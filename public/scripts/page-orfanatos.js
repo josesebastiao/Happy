dae2e19d111783ed7c3bbd3add54059a1e6a5973
José Afonso Sebastião <jose.sebastiao3@hotmail.com>
2020-10-23 23:13:46 -0300

    update

--- public/scripts/page-orfanatos.js
+++ public/scripts/page-orfanatos.js
@@ -1,1 +1,32 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+//Aqui criamos o mapa
+const map = L.map('mapid').setView([-14.9285358,13.5044904], 15);
+
+//Cria e adiciona o titleLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',).addTo(map);
+
+
+//Criação do icone
+const icon = L.icon({iconUrl:"./public/images/map-marker.svg",
+iconSize:[58,68],
+iconAnchor:[29,68],
+popupAnchor:[170, 2]
+
+})
+
+//Criação do Popup overlay
+const popup = L.popup({
+    closeButton:false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight:240
+
+}).setContent('Lar das meninas <a href="orfanatos.html?id=1"class="choose-orfanatos"> <img src="./public/images/arrow-white.svg"> </a>')
+
+
+
+//Esse codigo aqui ele coloca aquele pontinho de localização no mapa. O marcador e abre um popup
+L.marker([-14.9285358,13.5044904],{icon})
+.addTo(map)
+.bindPopup(popup)
+    
